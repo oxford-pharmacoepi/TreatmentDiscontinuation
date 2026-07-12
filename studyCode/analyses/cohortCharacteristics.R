c80dbb499ea1047224c045acccaf801636163fd3
catalamarti <marticatalasabate@gmail.com>
2026-07-13 00:44:43 +0100

    Update cohortCharacteristics.R

--- studyCode/analyses/cohortCharacteristics.R
+++ studyCode/analyses/cohortCharacteristics.R
@@ -7,3 +7,10 @@ result$attrition <- summariseCohortAttrition(cohort = cdm$study_cohorts)
 
 logMessage("Characterise study cohorts")
 result$characteristics <- summariseCharacteristics(cohort = cdm$study_cohorts)
+
+logMessage("Get code use of cohorts of interest")
+result$characteristics <- summariseCohortCodeUse(
+  cdm = cdm,
+  cohortTable = "study_cohorts",
+  cohortId = c("acute_mi", "beta_blockers_000")
+)
