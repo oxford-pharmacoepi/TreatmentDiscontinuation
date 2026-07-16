8b558661566577f754c166054c09079c871c19fe
catalamarti <marticatalasabate@gmail.com>
2026-07-16 10:09:14 +0100

    Update treatmentDiscontinuation.R

--- studyCode/analyses/treatmentDiscontinuation.R
+++ studyCode/analyses/treatmentDiscontinuation.R
@@ -2,7 +2,7 @@
 # survival
 logMessage("Run discontinuation as survival for mi")
 results$survival <- summariseDiscontinuationAsSurvival(
-  cohort = cdm$study_cohort,
+  cohort = cdm$study_cohorts,
   cohortId = sprintf("beta_blockers_%03i", gaps),
   followUpDays = 730,
   strata = strata
@@ -11,10 +11,10 @@ results$survival <- summariseDiscontinuationAsSurvival(
 # competing risk
 logMessage("Run discontinuation as competing risk")
 results$competing_risk <- summariseDiscontinuationAsSurvival(
-  cohort = cdm$study_cohort,
+  cohort = cdm$study_cohorts,
   cohortId = sprintf("beta_blockers_%03i", gaps),
   followUpDays = 730,
-  competingOutcomeCohortTable = "study_cohort",
+  competingOutcomeCohortTable = "study_cohorts",
   competingOutcomeCohortId = "death_cohort",
   strata = strata
 )
@@ -22,7 +22,7 @@ results$competing_risk <- summariseDiscontinuationAsSurvival(
 # proportion of patients covered
 logMessage("Run discontinuation as proportion of patients covered")
 results$ppc <- summariseProportionOfPatientsCovered(
-  cohort = cdm$study_cohort,
+  cohort = cdm$study_cohorts,
   cohortId = sprintf("beta_blockers_%03i", gaps),
   followUpDays = 730,
   strata = strata
@@ -31,14 +31,17 @@ results$ppc <- summariseProportionOfPatientsCovered(
 # multistate
 logMessage("Run discontinuation as multistate")
 for (gap in gaps) {
+  exposed <- sprintf("beta_blockers_%03i", gap)
+  unexposed <- sprintf("untreated_%03i", gap)
   tmat <- transMat(
     x = list(c(2, 3), c(1, 3), c()),
-    names = c(sprintf("beta_blockers_%03i", gap), sprintf("untreated_%03i", gap), "death_cohort")
+    names = c(exposed, unexposed, "death_cohort")
   )
-  results[[sprintf("multi_state_%03i", gap)]] <- summariseMultiStateProbabilities(
-    cohort = cdm$study_cohort,
-    tmat = tmat,
+  results[[sprintf("multi_state_%03i", gap)]] <- summariseMultistateProbabilities(
+    cohort = cdm$study_cohorts,
+    trans = tmat,
     followUpDays = 730,
-    strata = strata
+    strata = strata,
+    stateHierarchy = c("death_cohort", exposed, unexposed)
   )
 }
